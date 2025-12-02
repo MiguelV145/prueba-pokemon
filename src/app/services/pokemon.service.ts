6607bede89e484fe900d73ad3135b5b4fb2d517a
miguelv145 <miguelvanegas145@gmail.com>
2025-12-02 11:45:48 -0500

    2 oportunidad

--- src/app/services/pokemon.service.ts
+++ src/app/services/pokemon.service.ts
@@ -1,19 +1,22 @@
 import { Injectable, inject } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable, of, catchError, map } from 'rxjs';
-import { environment } from '../../environments/environment';
+// import { environment } from '../../environments/environment'; // <--- Puedes comentar esto si quieres
+
 import { PokemonDetail, PokemonListResponse } from '../features/home-page/interface-pok/interface-pokemon';
-
-
 
 @Injectable({ providedIn: 'root' })
 export class PokemonService {
   private http = inject(HttpClient);
-  private readonly API_URL = environment.apiUrl || 'https://pokeapi.co/api/v2';
+  
+  private readonly API_URL = 'https://pokeapi.co/api/v2'; 
 
-  // Lista paginada por offset & limit
+  // Lista paginada
   getPokemonList(offset = 0, limit = 20): Observable<PokemonListResponse> {
     const url = `${this.API_URL}/pokemon?offset=${offset}&limit=${limit}`;
+    
+    console.log('Solicitando a:', url); 
+
     return this.http.get<PokemonListResponse>(url).pipe(
       map(res => res),
       catchError(err => {
@@ -23,7 +26,6 @@ export class PokemonService {
     );
   }
 
-  // Detalle por id o nombre
   getPokemonById(idOrName: string | number): Observable<PokemonDetail | null> {
     const url = `${this.API_URL}/pokemon/${idOrName}`;
     return this.http.get<PokemonDetail>(url).pipe(
